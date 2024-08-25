76d98e6cb55dd5940f58d0585ff5f4a21091fa87
Laurent Stacul <laurent.stacul@gmail.com>
2024-08-25 15:30:39 +0200

    Use default styling provided by Org Babel

--- publish.el
+++ publish.el
@@ -58,12 +58,6 @@
              :section-numbers nil
              :time-stamp-file nil)))
 
-;; Customize the HTML output
-(setq org-html-validation-link nil            ;; Don't show validation link
-      org-html-head-include-scripts nil       ;; Use our own scripts
-      org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
-
 (org-publish-all t)
 
 (provide 'publish)
